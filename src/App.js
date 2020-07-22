6afa4cdc8dbe497b75b9ca63e4fd137e6167dbf9
Johnny <superstar41668087@gmail.com>
2020-07-22 01:35:07 -0400

    success test case

--- src/App.js
+++ src/App.js
@@ -1,14 +1,28 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
+function greet() {
+  let today = new Date();
+  let hours = today.getHours();
+  var greeting = "";
+  if (hours < 12) {
+    greeting = "Good morning";
+  } else if ((hours >= 12) & (hours < 18)) {
+    greeting = "Good afternoon";
+  } else {
+    greeting = "Good evening";
+  }
+  return greeting;
+}
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <p className="myGreet">{greet()}, everyone</p>
         <p>
-          This is travis Test <code>src/App.js</code> and save to reload.
+          <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,42 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, getByAltText, getByText } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("renders learn react link", () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+
+test("proper greeting sentence", () => {
+  const { container } = render(<App />);
+  const greeting = TDD_greet();
+
+  // const pElement = getByText(greeting);
+  // expect(getByText(greeting).textContent).toBe(greeting);
+
+  const para = container.getElementsByClassName("myGreet")[0];
+  expect(para.innerHTML.includes(greeting)).toBe(true);
+  // expect(paragraps[0])
+  // expect(container.find("myGreet").text().to.equal(greeting));
+  // target = container.getByText(greeting);
+
+  // greetClass = container.getElementsByClassName("myGreet");
+  // expect(container.contains("myGreet")).toBe(true);
+  // expect(container.contains(greeting)).toBe(true);
+  // const greetsentence = container.getElementsByClassName("myGreet");
+  // expect(greetsentence.contains(greeting)).toBe(true);
+});
+const TDD_greet = () => {
+  let today = new Date();
+  let hours = today.getHours();
+  var greeting = "";
+  if (hours < 12) {
+    greeting = "Good morning";
+  } else if ((hours >= 12) & (hours < 18)) {
+    greeting = "Good afternoon";
+  } else {
+    greeting = "Good evening";
+  }
+  return greeting;
+};
