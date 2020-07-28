26850ce414525f15af992392f80eafd6f12b9007
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-07-28 04:35:19 +0000

    Deploy Johnny-shin/travis to github.com/Johnny-shin/travis.git:gh-pages

---
+++ precache-manifest.c05ac16d0f8e5fe39bbb7736823acf11.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1477cb552536bc5aae0690a6869f46cb",
+    "url": "/travis/index.html"
+  },
+  {
+    "revision": "6a62fa03d51bc9f13155",
+    "url": "/travis/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "2d56dfa3ed13c5174727",
+    "url": "/travis/static/js/2.e2c2120a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/travis/static/js/2.e2c2120a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a62fa03d51bc9f13155",
+    "url": "/travis/static/js/main.c394373a.chunk.js"
+  },
+  {
+    "revision": "4e43f438450655f41d64",
+    "url": "/travis/static/js/runtime-main.0441ef66.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/travis/static/media/logo.5d5d9eef.svg"
+  }
+]);
