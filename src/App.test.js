da65840d33d4ef39c96452d69769ab0edbde7bbb
Johnny <superstar41668087@gmail.com>
2020-07-28 00:31:24 -0400

    The success test case!

--- src/App.test.js
+++ src/App.test.js
@@ -2,18 +2,20 @@ import React from "react";
 import { render, getByAltText, getByText } from "@testing-library/react";
 import App from "./App";
 
+//Test case 1
 test("renders learn react link", () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
 
+//Test case 2
 test("proper greeting sentence", () => {
   const { container } = render(<App />);
   const greeting = TDD_greet();
 
   const para = container.getElementsByClassName("myGreet")[0];
-  expect(para.innerHTML.includes(greeting)).toBe(true);
+  expect(para.innerHTML.includes(greeting)).toBe(true); //This line compare text in page with return result from TDD_greet()
 });
 const TDD_greet = () => {
   let today = new Date();
