23a8f44303aff54bf9cbe78bc007d97f5f00d60b
johnny <superstar41668087@gmail.com>
2021-11-21 03:11:53 -0500

    success test cases cicd

--- src/App.test.js
+++ src/App.test.js
@@ -1,5 +1,5 @@
+import { render } from "@testing-library/react";
 import React from "react";
-import { render, getByAltText, getByText } from "@testing-library/react";
 import App from "./App";
 
 //Test case 1
